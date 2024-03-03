15d571c86539eb97d0e58caebbfb678a2e50fe45
AyoubBIY <162037039+AyoubBIY@users.noreply.github.com>
2024-03-03 16:08:13 +0100

    Add files via upload

---
+++ MonBlog/login.php
@@ -0,0 +1,8 @@
+<form action="index.php?action=login" method="POST">
+    <input type="text" name="email" class="form__input" placeholder="email" required>
+    <input type="password" name="password" class="form__input" placeholder="Password" required>
+    <div>
+        <input type="submit" value="Se connecter">
+    </div>
+</form>
+<p><a href="register.php">S'inscrire maintenant</a></p>
